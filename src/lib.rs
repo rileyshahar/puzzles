09ba5edb56b046cee3f9efc323f54f08617c90cb
Riley Shahar <riley.shahar@gmail.com>
2021-12-17 23:30:05 -0800

    Remove destructuring_assignment feature gate
    It's on stable!

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,4 @@
-#![feature(
-    destructuring_assignment,
-    int_log,
-    map_try_insert,
-    step_trait,
-    test,
-    trait_alias
-)]
+#![feature(int_log, map_try_insert, step_trait, test, trait_alias)]
 
 pub mod aoc;
 pub mod euler;
