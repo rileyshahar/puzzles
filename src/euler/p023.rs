4059b7e9994bf5b6f7bd7a5c29fcedc429cad792
Riley Shahar <riley.shahar@gmail.com>
2022-05-02 19:56:30 -0700

    Optimize euler problem 23

--- src/euler/p023.rs
+++ src/euler/p023.rs
@@ -1,35 +1,25 @@
 //! Find the sum of all the positive integers which cannot be written as the sum of two abundant
 //! numbers.
-use std::cmp::Ordering;
-
 use crate::utils::seqs::Abundant;
-
-// Does the ordered list contain two elements which sum to the target?
-fn contains_two_element_sum(list: &[u32], target: u32) -> bool {
-    let mut begin = 0;
-    let mut end = list.len() - 1;
-
-    // successively update the upper and lower bounds on the elements that can be involved in the
-    // sum, until we hit the correct sum
-    while begin < end {
-        let curr = list[begin] + list[end];
-        match curr.cmp(&target) {
-            Ordering::Less => begin += 1,
-            Ordering::Equal => return true,
-            Ordering::Greater => end -= 1,
-        }
-    }
-
-    // special case for begin = end, in case we have end = 0, when we can't subtract because of
-    // underflow
-    list[begin] + list[end] == target
-}
 
 fn solve_for(max: u32) -> u32 {
     let abundants: Vec<_> = Abundant::<u32>::new().take_while(|&n| n < max).collect();
 
+    let mut is_sum_of_two_abundants = vec![false; max as usize];
+
+    for (i, &a) in abundants.iter().enumerate() {
+        for b in abundants[i..].iter() {
+            let sum = a + b;
+            if sum >= max {
+                // clearly no more sums for this a will be relevant to us
+                break;
+            }
+            is_sum_of_two_abundants[sum as usize] = true;
+        }
+    }
+
     (1..max)
-        .filter(|&n| !contains_two_element_sum(&abundants, n))
+        .filter(|&i| !is_sum_of_two_abundants[i as usize])
         .sum()
 }
 
